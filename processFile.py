423dcd3ac04e43c4f614601da533de9c0b9365a8
Shrey Mandol <mandol@ualberta.ca>
2024-01-26 00:55:12 -0700

    update scores file

--- processFile.py
+++ processFile.py
@@ -17,16 +17,19 @@ def find_latest_csv(folder_path):
 
 # Read CSV file and insert data into player_daily_performance table
 def insert_data_from_csv(csv_file):
+    date = datetime.now().date()
+    conn = sqlite3.connect('SI_2024_FANTASY_LEAGUE.db')
+    cursor = conn.cursor()
     with open(csv_file, 'r', encoding='utf-8-sig') as file:
         # Connect to the SQLite database
-        conn = sqlite3.connect('SI_2024_FANTASY_LEAGUE.db')
-        cursor = conn.cursor()
+
         csv_reader = csv.DictReader(file)
+        
         for row in csv_reader:
             # Extract relevant data from the CSV row
             player_name = row['Player'].lower()
             team_name = row['Team'].lower()
-            date = datetime.now().date()  # Assuming you want to use the current date
+              # Assuming you want to use the current date
             body_shot_kills = int(row['Body Shot Kills'])
             headshot_kills = int(row['Entry Kills'])
             utility_kills = int(row.get('Utility Kills', 0))  # Assuming 'Utility Kills' may not be present in every row
@@ -48,9 +51,38 @@ def insert_data_from_csv(csv_file):
                     ((SELECT player_id FROM players WHERE player_name = ? AND team_name = ?), ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
             ''', (player_name, team_name, date, body_shot_kills, headshot_kills, utility_kills, opening_kills, deaths, opening_deaths, round_wins, plant_points, total_points, points_per_round))
             
-        # Commit changes and close the connection
-        conn.commit()
-        conn.close()
+            
+    # Get all the managers' score for the day and insert it into the managers_daily_scores table
+    all_manager_ids = cursor.execute('SELECT manager_id FROM managers WHERE in_closed = TRUE').fetchall()
+    for manager_id in all_manager_ids:
+        manager_id = manager_id[0]
+        manager_score = cursor.execute('''
+            SELECT SUM(total_points) FROM player_daily_performance 
+            WHERE player_id IN (SELECT player_id FROM closed_game_teams WHERE manager_id = ? and is_active = True) AND date = ?
+        ''', (manager_id, date)).fetchone()[0]
+        cursor.execute('''
+            INSERT INTO managers_daily_scores 
+                (manager_id, date, closed_game_score, created_at, updated_at) 
+            VALUES 
+                (?, ?, ?, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+        ''', (manager_id, date, manager_score))
+    all_open_manager_ids = cursor.execute('SELECT manager_id FROM managers WHERE in_closed = FALSE').fetchall()
+    for manager_id in all_open_manager_ids:
+        manager_id = manager_id[0]
+        manager_score = cursor.execute('''
+            SELECT SUM(total_points) FROM player_daily_performance 
+            WHERE player_id IN (SELECT player_id FROM open_game_teams WHERE manager_id = ? and is_active = True) AND date = ?
+        ''', (manager_id, date)).fetchone()[0]
+        cursor.execute('''
+            INSERT INTO managers_daily_scores 
+                (manager_id, date, open_game_roster, created_at, updated_at) 
+            VALUES 
+                (?, ?, ?, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+        ''', (manager_id, date, manager_score))
+            
+    # Commit changes and close the connection
+    conn.commit()
+    conn.close()
 
 
 # Handler class for file system events
